1681fa01a4c9d48283e1dedd87d69769f6c02554
Daniel Kim <dkim2707@gmail.com>
2016-08-11 00:49:16 -0700

    Switch ES6 arrow function for ES5

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ var app = express();
 const PORT =  process.env.PORT || 3000;
 
 // open weather map API, open weather map only operates on HTTP and not HTTPS.
-app.use((req, res, next) => {
+app.use((function (req, res, next) {
   if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
     res.redirect('http://' + req.hostname + req.url);
   }
 });
-console.log(PORT);
+
 app.use(express.static('public'));
 
-app.listen(PORT, () => {
+app.listen(PORT, function () {
   console.log('Listening on port ' + PORT);
 });
