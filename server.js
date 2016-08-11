a93ed488ee306c63abc4fbb465f6395b34e71c89
Daniel Kim <dkim2707@gmail.com>
2016-08-11 00:21:48 -0700

    prep repo for heroku

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-var express = require('express');
+let express = require('express');
+let app = express();
+const PORT =  process.env.PORT;
 
-var app = express();
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    req.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000')
+app.listen(PORT, () => {
+  console.log('Listening on port ', PORT);
 });
