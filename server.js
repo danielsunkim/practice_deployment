2d896776c76943920981575aee8eda29496b4a1b
Daniel Kim <dkim2707@gmail.com>
2016-08-11 00:25:57 -0700

    Correct typo in server

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ let express = require('express');
 let app = express();
 const PORT =  process.env.PORT;
 
+// open weather map API, open weather map only operates on HTTP and not HTTPS.
 app.use((req, res, next) => {
   if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
-    req.redirect('http://' + req.hostname + req.url);
+    res.redirect('http://' + req.hostname + req.url);
   }
 });
 
